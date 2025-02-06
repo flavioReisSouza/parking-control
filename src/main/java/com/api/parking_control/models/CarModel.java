1b476838276d93767fb8fdc82982dc31e32710d6
Flavio Reis <flavioreissouza@yahoo.com>
2025-02-05 22:33:48 -0300

    feat: add ORM mapping for Car and ParkingSpot entities with repositories

--- src/main/java/com/api/parking_control/models/CarModel.java
+++ src/main/java/com/api/parking_control/models/CarModel.java
@@ -13,15 +13,14 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Table(name = "TB_PARKING_SPOT")
+@Table(name = "TB_CAR")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ParkingSpotModel implements Serializable {
+public class CarModel implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
@@ -30,21 +29,18 @@ public class ParkingSpotModel implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
-    @Column(nullable = false, unique = true, length = 10)
-    private String parkingSpotNumber;
+    @Column(nullable = false, unique = true, length = 7)
+    private String licensePlateCar;
 
-    @Column(nullable = false)
-    private LocalDateTime registrationDate;
+    @Column(nullable = false, length = 70)
+    private String brandCar;
 
-    @Column(nullable = false, length = 130)
-    private String responsibleName;
+    @Column(nullable = false, length = 70)
+    private String modelCar;
 
-    @Column(nullable = false, length = 30)
-    private String apartmentNumber;
+    @Column(nullable = false, length = 70)
+    private String colorCar;
 
-    @Column(nullable = false, length = 30)
-    private String block;
-
-    @OneToOne
-    private CarModel car;
+    @OneToOne(mappedBy = "car")
+    private ParkingSpotModel parkingSpot;
 }
